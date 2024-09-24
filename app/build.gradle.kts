45eabd5ae72772f0163a091ff7f21bed8f3b7018
farsuller <farsuller2020@gmail.com>
2024-09-24 16:21:11 +0800

    update theme, added google cert and color schemes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,8 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
 
+    implementation(libs.androidx.ui.text.google.fonts)
+
     //Lifecycle
     implementation (libs.androidx.lifecycle.viewmodel.compose)
 
--- app/src/main/java/com/solodev/mindpad/MainActivity.kt
+++ app/src/main/java/com/solodev/mindpad/MainActivity.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.solodev.mindpad.ui.theme.MindPadTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,7 +24,7 @@ class MainActivity : ComponentActivity() {
             MindPadTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "RENZ",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -32,16 +35,37 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column {
+
+        Text(
+            text = "Hello $name!",
+            fontFamily = MaterialTheme.typography.displayLarge.fontFamily,
+            fontSize = MaterialTheme.typography.displayLarge.fontSize,
+            fontWeight = MaterialTheme.typography.displayLarge.fontWeight,
+            modifier = modifier
+        )
+        Text(
+            text = "Hello $name!",
+            fontFamily = MaterialTheme.typography.displayMedium.fontFamily,
+            fontSize = MaterialTheme.typography.displayMedium.fontSize,
+            fontWeight = MaterialTheme.typography.displayMedium.fontWeight,
+            modifier = modifier
+        )
+
+        Text(
+            text = "Hello $name!",
+            fontFamily = MaterialTheme.typography.displaySmall.fontFamily,
+            fontSize = MaterialTheme.typography.displaySmall.fontSize,
+            fontWeight = MaterialTheme.typography.displaySmall.fontWeight,
+            modifier = modifier
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MindPadTheme {
-        Greeting("Android")
+        Greeting("RENZ")
     }
 }
